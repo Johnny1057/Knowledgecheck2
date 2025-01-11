6cb5e34d434fed1a26905211200f871bd747e01c
Johnny <johnnyjohnson829@gmail.com>
2025-01-11 17:29:29 -0500

    I did it!

---
+++ Knowledgecheck2sl/TShirt.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Knowledgecheck2sl
+{
+    public class TShirt : ClothingItem
+    {
+        public string Design { get; set; } = string.Empty;
+        public string SleeveType { get; set; } = string.Empty;
+
+        public TShirt() { }
+    }
+}
